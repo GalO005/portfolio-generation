e19c7d1fb31ad9cf1589b40b9f86668fa22869da
Gal Oz <galoz05@gmail.com>
2025-02-10 13:16:24 +0200

    ✨ add cors middleware to enable cross-origin resource sharing

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application } from "express";
 import initializeDB from "./config/db";
 import router from "./routes";
+import cors from "cors";
 
 const app: Application = express();
 
 
 initializeDB();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", router);
 
