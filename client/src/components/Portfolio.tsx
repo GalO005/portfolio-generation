dc906fe75f40aa77605b83cee6d2ed25a00a62b1
Gal Oz <44542292+GalO005@users.noreply.github.com>
2025-02-10 13:53:29 +0200

    Feature/create integration client server (#9)
    * ✨ add API routes for portfolio management; initialize router and define generate endpoint
    
    * ✨ add cors middleware to enable cross-origin resource sharing
    
    * ✨ add axios for API requests and implement portfolio fetching in Portfolio component

--- client/src/components/Portfolio.tsx
+++ client/src/components/Portfolio.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import axiosInstance from "@/api/axiosInstance";
+
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
 
 interface Project {
@@ -13,16 +15,29 @@ interface Project {
     available: number;
     distribution_weight: number;
   }
-const projects: Project[] = [
-    { id: 1, name: "EverGreen CarbonScape", country: "Germany", image: "https://ceezer-public-assets.s3.eu-central-1.amazonaws.com/project_type_sample_images/Fugitives/38bb530f5caf513be9f2a41f2d909f47-min.jpeg", price_per_ton: 650, available: 15, distribution_weight: 0.05 },
-    { id: 2, name: "VerdeCarbon", country: "India", image: "https://ceezer-public-assets.s3.eu-central-1.amazonaws.com/project_type_sample_images/Mineralisation/ben-karpinski-ctWw2S9VqOI-unsplash-min.jpg", price_per_ton: 200, available: 900, distribution_weight: 0.1 },
-    { id: 3, name: "SustainaForest Carbon", country: "Brazil", image: "https://ceezer-public-assets.s3.eu-central-1.amazonaws.com/project_type_sample_images/Afforestation+reforestation/marita-kavelashvili-ugnrXk1129g-unsplash-min.jpg", price_per_ton: 50.85, available: 1500, distribution_weight: 0.15 },
-    { id: 4, name: "EcoRespire", country: "India", image: "https://ceezer-public-assets.s3.eu-central-1.amazonaws.com/project_type_sample_images/Climate+fund/climate-fund.png", price_per_ton: 25, available: 1100, distribution_weight: 0.25 },
-    { id: 5, name: "EverGreen Carbon", country: "Egypt", image: "https://ceezer-public-assets.s3.eu-central-1.amazonaws.com/project_type_sample_images/Renewable+energy/andreas-gucklhorn-Ilpf2eUPpUE-unsplash-min.jpg", price_per_ton: 10.5, available: 16000, distribution_weight: 0.45 }
-  ];
 
 export default function Portfolio() {
   const [requestedTons, setRequestedTons] = useState("");
+  const [portfolio, setPortfolio] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const fetchPortfolio = async () => {
+    setLoading(true);
+    setError('');
+    try {
+      const response = await axiosInstance.post("/portfolio/generate", {
+        requestedTons: requestedTons,
+      });
+      console.log(response.data);
+      
+      //setPortfolio(response.data);
+    } catch (err) {
+      setError("Failed to fetch portfolio");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="p-4 max-w-xl mx-auto">
       <Card>
@@ -36,10 +51,10 @@ export default function Portfolio() {
             placeholder="Enter desired tons"
             className="mb-4"
           />
-          <Button onClick={() => console.log("Generate Portfolio")}>Generate Portfolio</Button>
+          <Button onClick={fetchPortfolio}>Generate Portfolio</Button>
         </CardContent>
       </Card>
-      {projects.length > 0 && (
+      {portfolio.length > 0 && (
         <Table className="mt-4">
           <TableHeader>
             <TableRow>
@@ -49,7 +64,7 @@ export default function Portfolio() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {projects.map((p:Project) => (
+            {portfolio.map((p:Project) => (
               <TableRow key={p.id}>
                 <TableCell>{p.name}</TableCell>
                 <TableCell>{p.country}</TableCell>
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
 import express, { Application } from "express";
 import initializeDB from "./config/db";
+import router from "./routes";
+import cors from "cors";
 
 const app: Application = express();
 
 
 initializeDB();
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", router);
 
 //Sample route
 app.get("/", (req, res) => {
